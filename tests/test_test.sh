5ba6d2d7ca8ed9606a45cae9054ee2a4151df055
Gracemc <wmc-1128@163.com>
2017-05-31 17:38:05 -0700

    Create test_test.sh

---
+++ tests/test_test.sh
@@ -0,0 +1,10 @@
+touch newfile
+touch file2
+[ -d newfile ]
+test -f newfile
+test -e rshell && echo "path exists" || test -e file2
+test -e file || test -e file2
+[ -e rshell ] && echo path exists
+[ -e rshell ] ]
+[ [ -e newfile ] ]
+exit
